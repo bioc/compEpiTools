089c77ccfc5885c7224494c08a1c324e65723233
Kamal Kishore <kamal.fartiyal84@gmail.com>
2014-10-13 13:24:27 +0000

    GRcoverageSummit bug fixed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/compEpiTools@95386 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GRcoverageSummit.R
+++ R/GRcoverageSummit.R
@@ -12,9 +12,14 @@ setMethod('GRcoverageSummit','GRanges', function(Object, bam) {
     for(i in 1:length(maxPos)) maxPos[[i]] <- maxPos[[i]] + starts[i] - 1
     maxPosL <- sapply(maxPos, length)
                                         # if multiple summits are found, take one random
-    if(max(maxPosL) > 1) maxPos[maxPosL > 1] <-
+    if(max(maxPosL) > 1) 
+      {
+      maxPos[maxPosL > 1] <-
         sapply(maxPos[maxPosL > 1], function(x) sample(x, 1))
-    else maxPos <- unlist(maxPos)
+      maxPos <- unlist(maxPos)
+      }
+    else 
+      maxPos <- unlist(maxPos)
     start(Object) <- maxPos
     end(Object) <- maxPos
 
