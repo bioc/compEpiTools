156e675d0d0ed1dbb6e2c767ac7ba214719ef191
Kamal Kishore <kamal.fartiyal84@gmail.com>
2015-05-21 14:49:05 +0000

    Updated transcripts retrieval in all functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/compEpiTools@104038 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GRangesInPromoters.R
+++ R/GRangesInPromoters.R
@@ -1,35 +1,35 @@
 setGeneric('GRangesInPromoters', function(Object, txdb, upstream=2000,
                                           downstream=1000, invert=FALSE)
-           standardGeneric('GRangesInPromoters'))
+  standardGeneric('GRangesInPromoters'))
 setMethod('GRangesInPromoters','GRanges', function(Object, txdb,
                                                    upstream=2000, downstream=1000, invert=FALSE) {
-
-    if(!is(txdb, "TxDb"))
-        stop('txdb has to be of class TxDb ..')
-    if(!is.numeric(upstream))
-        stop('upstream has to be of class numeric ..')
-    if(!is.numeric(downstream))
-        stop('downstream has to be of class numeric ..')
-    if(!is.logical(invert))
-        stop('invert has to be of class logical ...')
-
-    txbygene <- transcriptsBy(txdb, by='gene')
-    suppressWarnings(pregions <- unlist(promoters(txbygene,
-                                                  upstream=upstream, downstream=downstream)))
-    inds <- overlapsAny(query= Object,
-			subject= pregions, minoverlap=1L, type='any')
-    if(invert) {
-        inds_f <- which(inds==FALSE)
-        if(length(inds_f) > 0)
-            res <- Object[inds_f]
-        else
-            res <- NULL
-    }
-    else {
-        inds_t <- which(inds==TRUE)
-        if(length(inds_t) > 0) res <- Object[inds_t]
-        else res <- NULL
-    }
-    return(res)
+  
+  if(!is(txdb, "TxDb"))
+    stop('txdb has to be of class TxDb ..')
+  if(!is.numeric(upstream))
+    stop('upstream has to be of class numeric ..')
+  if(!is.numeric(downstream))
+    stop('downstream has to be of class numeric ..')
+  if(!is.logical(invert))
+    stop('invert has to be of class logical ...')
+  
+  suppressWarnings(pregions <- promoters(txdb, upstream=upstream, downstream=downstream, 
+                                         columns=c('tx_id','tx_name','gene_id')))
+  
+  inds <- overlapsAny(query= Object,
+                      subject= pregions, minoverlap=1L, type='any')
+  if(invert) {
+    inds_f <- which(inds==FALSE)
+    if(length(inds_f) > 0)
+      res <- Object[inds_f]
+    else
+      res <- NULL
+  }
+  else {
+    inds_t <- which(inds==TRUE)
+    if(length(inds_t) > 0) res <- Object[inds_t]
+    else res <- NULL
+  }
+  return(res)
 })
 
