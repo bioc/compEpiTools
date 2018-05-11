87a5d8924d3ce8dbd8262254f1f182ec44da2c68
kamalfartiyal84 <kamal.fartiyal84@gmail.com>
2018-05-11 22:59:54 +0100

    updated heatmapplot and topGOres

--- R/heatmapPlot.R
+++ R/heatmapPlot.R
@@ -1,166 +1,160 @@
-heatmapPlot <- function(matList, sigMat=NULL, qnorm=NULL, tnorm=NULL,
-                        rowLab=FALSE, colLab=TRUE, margins=NULL, colors=NULL,
-                        clusterInds=1:length(matList), dendrogram=TRUE) {
-    if(!is(matList,'list'))
-        stop('matList has to be of class list ...')
-    if(!all(sapply(matList, class) == 'matrix'))
-        stop('matList has to be a list of matrices ...')
-    if(!all(sapply(matList, ncol) == ncol(matList[[1]])))
-        stop('matList has to be a list of matrices with
-        the same number of columns ...')
-    if(any(sapply(matList, min, na.rm=TRUE) < 0))
-        stop('matList cannot contain negative counts ...')
-    if(!is.null(sigMat) && !is(sigMat,'matrix'))
-        stop('sigMat has to be of class matrix ...')
-    if(!is.null(sigMat) && (nrow(sigMat) != nrow(matList[[1]]) ||
-                            ncol(sigMat) != length(matList)))
-        stop('sigMat does not have correct dimensions ...')
-    if(!is.null(sigMat) && (min(sigMat) < 0 || max(sigMat) > 1))
-        stop('sigMat has to range in [0,1] ...')
-    if(!is.null(qnorm) && (!is.numeric(qnorm) ||
-                           length(qnorm) != length(matList)))
-        stop('qnorm has to be either NULL or an array of numeric having
-        the same length as matList ...')
-    if(!is.null(tnorm) && (!is.numeric(tnorm) ||
-                           length(tnorm) != length(matList)))
-        stop('tnorm has to be either NULL or of an array of numeric having
-            the same length as matList ...')
-    if(length(qnorm) > 1 && length(tnorm) > 1 &&
-       length(intersect(which(is.null(qnorm)), which(is.null(tnorm)))) > 0)
-        warning('some matList items will be normalized according to
-        both qnorm and tnorm ...')
+function (matList, sigMat = NULL, qnorm = NULL, tnorm = NULL, 
+    rowLab = FALSE, colLab = TRUE, margins = NULL, colors = NULL, 
+    clusterInds = 1:length(matList), dendrogram = TRUE) 
+{
+    if (!is(matList, "list")) 
+        stop("matList has to be of class list ...")
+    if (!all(sapply(matList, class) == "matrix")) 
+        stop("matList has to be a list of matrices ...")
+    if (!all(sapply(matList, ncol) == ncol(matList[[1]]))) 
+        stop("matList has to be a list of matrices with\n        the same number of columns ...")
+    if (any(sapply(matList, min, na.rm = TRUE) < 0)) 
+        stop("matList cannot contain negative counts ...")
+    if (!is.null(sigMat) && !is(sigMat, "matrix")) 
+        stop("sigMat has to be of class matrix ...")
+    if (!is.null(sigMat) && (nrow(sigMat) != nrow(matList[[1]]) || 
+        ncol(sigMat) != length(matList))) 
+        stop("sigMat does not have correct dimensions ...")
+    if (!is.null(sigMat) && (min(sigMat) < 0 || max(sigMat) > 
+        1)) 
+        stop("sigMat has to range in [0,1] ...")
+    if (!is.null(qnorm) && (!is.numeric(qnorm) || length(qnorm) != 
+        length(matList))) 
+        stop("qnorm has to be either NULL or an array of numeric having\n        the same length as matList ...")
+    if (!is.null(tnorm) && (!is.numeric(tnorm) || length(tnorm) != 
+        length(matList))) 
+        stop("tnorm has to be either NULL or of an array of numeric having\n            the same length as matList ...")
+    if (length(qnorm) > 1 && length(tnorm) > 1 && length(intersect(which(is.null(qnorm)), 
+        which(is.null(tnorm)))) > 0) 
+        warning("some matList items will be normalized according to\n        both qnorm and tnorm ...")
     li <- length(matList)
-    if(names(matList)[li] == 'genes -' && (!is.null(qnorm[[li]])
-                         || !is.null(tnorm[[li]])))
+    if (names(matList)[li] == "genes -" && (!is.null(qnorm[[li]]) || 
+        !is.null(tnorm[[li]]))) 
         warning("'genes +' and 'genes -' might be normalized ...")
-    if(!is.logical(rowLab) || !is.logical(colLab))
-        stop('colLab and rowLab have to be of class logical ...')
-    if(!is.null(margins) && !is.numeric(margins))
-        stop('margins has to be either NULL or a numeric array of length 2...')
-    if(!is.null(colors) && !is.character(colors))
-        stop('colors has to be either NULL or of class character...')
-    if(is.numeric(margins) && length(margins) != 2)
-        stop('margins has to be either NULL or a numeric array of length 2 ...')
-    if(!is.null(clusterInds) && !is.numeric(clusterInds))
-        stop('clusterInds has to be either NULL or of class numeric ...')
-    if(!is.null(clusterInds) && !all(clusterInds %in% 1:length(matList)))
-        stop('clusterInds has to be either NULL or an array containing
-        indexes to matList items ...')
-    if(!is.logical(dendrogram))
-        stop('dendrogram has to be of class logical ...')
-
-    if(names(matList)[li] == 'genes -' && !is.null(sigMat))
-        sigMat[,(ncol(sigMat) - 1):ncol(sigMat)] <- 0
-
-   # normalization
-    if(!is.null(qnorm)) {
-        for(i in 1:length(qnorm)) {
-            if(is.null(qnorm[i])) next
-            maxQ <- quantile(matList[[i]], qnorm[[i]], na.rm=TRUE)
+    if (!is.logical(rowLab) || !is.logical(colLab)) 
+        stop("colLab and rowLab have to be of class logical ...")
+    if (!is.null(margins) && !is.numeric(margins)) 
+        stop("margins has to be either NULL or a numeric array of length 2...")
+    if (!is.null(colors) && !is.character(colors)) 
+        stop("colors has to be either NULL or of class character...")
+    if (is.numeric(margins) && length(margins) != 2) 
+        stop("margins has to be either NULL or a numeric array of length 2 ...")
+    if (!is.null(clusterInds) && !is.numeric(clusterInds)) 
+        stop("clusterInds has to be either NULL or of class numeric ...")
+    if (!is.null(clusterInds) && !all(clusterInds %in% 1:length(matList))) 
+        stop("clusterInds has to be either NULL or an array containing\n        indexes to matList items ...")
+    if (!is.logical(dendrogram)) 
+        stop("dendrogram has to be of class logical ...")
+    if (names(matList)[li] == "genes -" && !is.null(sigMat)) 
+        sigMat[, (ncol(sigMat) - 1):ncol(sigMat)] <- 0
+    if (!is.null(qnorm)) {
+        for (i in 1:length(qnorm)) {
+            if (is.null(qnorm[i])) 
+                next
+            maxQ <- quantile(matList[[i]], qnorm[[i]], na.rm = TRUE)
             matList[[i]][matList[[i]] > maxQ] <- maxQ
-            matList[[i]] <- matList[[i]] / maxQ
+            matList[[i]] <- matList[[i]]/maxQ
         }
     }
-    if(!is.null(tnorm)) {
-        for(i in 1:length(tnorm)) {
-            if(is.null(tnorm[i])) next
+    if (!is.null(tnorm)) {
+        for (i in 1:length(tnorm)) {
+            if (is.null(tnorm[i])) 
+                next
             matList[[i]][matList[[i]] > tnorm[[i]]] <- tnorm[[i]]
-            matList[[i]] <- matList[[i]] / tnorm[[i]]
+            matList[[i]] <- matList[[i]]/tnorm[[i]]
         }
     }
-
-    # normalizing to 1 (dividing by the maximum) the matList entries
-    # not normalized with qnorm or tnorm
     notNorm <- which(is.null(qnorm) & is.null(tnorm))
-    if(length(notNorm) > 0) {
-        for(i in 1:length(matList)) {
-            if(max(matList[[i]], na.rm=TRUE) > 1) {
-                matList[[i]] <- matList[[i]]/max(matList[[i]], na.rm=TRUE)
+    if (length(notNorm) > 0) {
+        for (i in 1:length(matList)) {
+            if (max(matList[[i]], na.rm = TRUE) > 1) {
+                matList[[i]] <- matList[[i]]/max(matList[[i]], 
+                  na.rm = TRUE)
                 matList[[i]][matList[[i]] > 1] <- 1
             }
         }
     }
-
-   # assembling the matrix for the heatmap
     mat <- NULL
-    for(i in 1:length(matList)) mat <- cbind(mat, matList[[i]])
+    for (i in 1:length(matList)) mat <- cbind(mat, matList[[i]])
 
-   # clustering
-    if(!is.null(clusterInds)) {
+
+
+    if (!is.null(clusterInds)) {
         clmat <- NULL
-        for(i in clusterInds) clmat <- cbind(clmat, matList[[i]])
+        for (i in clusterInds) clmat <- cbind(clmat, matList[[i]])
         NAcounts <- apply(mat, 1, function(x) length(which(is.na(x))))
-        NAinds <- which(NAcounts > ncol(mat) * 0.30)
-        if(length(NAinds) == nrow(mat)) return(NULL)
-        if(length(NAinds) > 0) { # all NA rows are discarded
-            mat <- mat[-NAinds,]
-            clmat <- clmat[-NAinds,]
+        NAinds <- which(NAcounts > ncol(mat) * 0.3)
+        if (length(NAinds) == nrow(mat)) 
+            return(NULL)
+        if (length(NAinds) > 0) {
+            mat <- mat[-NAinds, ]
+            clmat <- clmat[-NAinds, ]
         }
-        clRes <- hclust(dist(clmat), method = "average")
-        rowv <- as.dendrogram(hclust(dist(clmat), method = "average"))
-        if(dendrogram) dendr <- 'row'
-        else dendr <- 'none'
+		clRes <- hclust(dist(clmat), method = "average")
+        rowv <- as.dendrogram(clRes)
+        if (dendrogram) 
+            dendr <- "row"
+        else dendr <- "none"
     }
     else {
         rowv <- FALSE
-        dendr <- 'none'
+        dendr <- "none"
     }
-
-    # row and column labels
-    if(rowLab) {
+    if (rowLab) {
         rowlab <- rownames(mat)
         rmar <- 8
     }
-    else {rowlab <- rowLab; rmar <- 4}
-    if(colLab) {
+    else {
+        rowlab <- rowLab
+        rmar <- 4
+    }
+    if (colLab) {
         midCol <- round(ncol(matList[[1]])/2)
         labCol <- cumsum(sapply(matList, ncol)) - midCol
-        collab <- rep('', ncol(mat))
+        collab <- rep("", ncol(mat))
         collab[labCol] <- names(matList)
         cmar <- 8
     }
-    else collab <- colLab; cmar <- 4
-    if(is.null(margins)) margins <- c(cmar,rmar)
-
-    # plotting
-    if(!is.null(sigMat)) {
-        # 30 levels for intensity and 10 levels for significance
-        colmat <- palette2d(n=30, k=10)
+    else collab <- colLab
+    cmar <- 4
+    if (is.null(margins)) 
+        margins <- c(cmar, rmar)
+    if (!is.null(sigMat)) {
+        colmat <- palette2d(n = 30, k = 10)
         cols <- as.vector(t(colmat))
         colbreaks <- 0:300
         sigMat[sigMat < 1e-10] <- 1e-10
         scaledSig <- round(-log10(sigMat))
-        scaledSig[scaledSig > 0] <- scaledSig[scaledSig > 0]- 1
+        scaledSig[scaledSig > 0] <- scaledSig[scaledSig > 0] - 
+            1
         scaledSigMat <- NULL
         nbins <- ncol(matList[[1]])
-        for(i in 1:ncol(scaledSig)) {
-            for(j in 1:nbins) scaledSigMat <- cbind(scaledSigMat, scaledSig[,i])
+        for (i in 1:ncol(scaledSig)) {
+            for (j in 1:nbins) scaledSigMat <- cbind(scaledSigMat, 
+                scaledSig[, i])
         }
-        mat <- round(1 + mat*29)
-        mat <- mat + 30*scaledSigMat
+        mat <- round(1 + mat * 29)
+        mat <- mat + 30 * scaledSigMat
     }
     else {
-        drange <- range(mat, na.rm=TRUE)
-        colbreaks <- seq(drange[1], drange[2], length.out=129)
-        if(is.null(colors))
-        {
-          col12 <- colorRampPalette(c('white','beige'))
-          col23 <- colorRampPalette(c('beige','red'))
-          cols <- c(col12(64), col23(64))
+        drange <- range(mat, na.rm = TRUE)
+        colbreaks <- seq(drange[1], drange[2], length.out = 129)
+        if (is.null(colors)) {
+            col12 <- colorRampPalette(c("white", "beige"))
+            col23 <- colorRampPalette(c("beige", "red"))
+            cols <- c(col12(64), col23(64))
         }
-        else
-        {
-          col12 <- colorRampPalette(colors)
-          cols <- c(col12(128))
+        else {
+            col12 <- colorRampPalette(colors)
+            cols <- c(col12(128))
         }
     }
-
     colSep <- c(0, cumsum(sapply(matList, ncol)))
-    res <- heatmap.2(x=mat, breaks=colbreaks, Colv=FALSE, Rowv=rowv, dendrogram=dendr,
-             scale="none", col=cols, density.info="none", symkey=FALSE,
-             trace="none", margins=margins, labRow=rowlab, labCol=collab,
-             sepcolor='black', colsep=colSep, sepwidth=0.05,
-             na.color='grey')
-
-    invisible(list(data= mat, heatRes=res))
+    res <- heatmap.2(x = mat, breaks = colbreaks, Colv = FALSE, 
+        Rowv = rowv, dendrogram = dendr, scale = "none", col = cols, 
+        density.info = "none", symkey = FALSE, trace = "none", 
+        margins = margins, labRow = rowlab, labCol = collab, 
+        sepcolor = "black", colsep = colSep, sepwidth = 0.05, 
+        na.color = "grey")
+    invisible(list(data = mat, heatRes = res))
 }
--- R/topGOres.R
+++ R/topGOres.R
@@ -1,63 +1,89 @@
-topGOres <- function(ids, ontology='BP', Pthr=1e-5, maxN=5000,
-                     minN=5, orgdb, allEG=keys(orgdb), showTerms=NULL) {
-    if(!is.character(ids))
-        stop('ids has to be of class character ...')
-    if(!all(ontology %in% c('BP','CC','MF')))
-        stop('ontology has to be one of: CC, BP, MF ...')
-    if(!is.numeric(Pthr))
-        stop('Pthr has to be of class character ...')
-    if(!is.numeric(maxN))
-        stop('maxN has to be of class character ...')
-    if(!is.numeric(minN))
-        stop('minN has to be of class character ...')
-    if(!is(orgdb,'OrgDb'))
-        stop('orgdb has to be of class OrgDb ...')
-    if(species(orgdb) != 'Homo sapiens' && species(orgdb) != 'Mus musculus' && species(orgdb) != 'Drosophila melanogaster')
-        stop('only org.Hs.eg.db, org.Mm.eg.db and org.Dm.eg.db are supported ...')
-    if(!is(allEG, 'character'))
-        stop('allEG has to be of class character ...')
-    if(!is.null(showTerms) && !is.numeric(showTerms))
-      stop('showTerms has to be either NULL or of class numeric ...')
-    
-    EGvec <- unique(c(allEG, ids))
+topGOres2 <- function (ids, ontology = "BP", Pthr = 1e-05, maxN = 5000, minN = 5, 
+    orgdb, allEG = keys(orgdb), showTerms = NULL) 
+{
+    if (!is.list(ids) & !is.character(ids))
+        stop("ids has to be of class character or list ...")
+    if (is.list(ids) & !is.character(unlist(ids))) 
+        stop("elements of ids has to be of class character ...")
+    if (!all(ontology %in% c("BP", "CC", "MF"))) 
+        stop("ontology has to be one of: CC, BP, MF ...")
+    if (!is.numeric(Pthr)) 
+        stop("Pthr has to be of class character ...")
+    if (!is.numeric(maxN)) 
+        stop("maxN has to be of class character ...")
+    if (!is.numeric(minN)) 
+        stop("minN has to be of class character ...")
+    if (!is(orgdb, "OrgDb")) 
+        stop("orgdb has to be of class OrgDb ...")
+    if (species(orgdb) != "Homo sapiens" && species(orgdb) != 
+        "Mus musculus" && species(orgdb) != "Drosophila melanogaster") 
+        stop("only org.Hs.eg.db, org.Mm.eg.db and org.Dm.eg.db are supported ...")
+    if (!is(allEG, "character")) 
+        stop("allEG has to be of class character ...")
+    if (!is.null(showTerms) && !is.numeric(showTerms)) 
+        stop("showTerms has to be either NULL or of class numeric ...")
+    if (!is.list(ids))
+        ids <- list(ids)
+    EGvec <- unique(c(allEG, unlist(ids)))
     EGvec <- rep(0, length(allEG))
     names(EGvec) <- allEG
-    EGvec[ids] <- 1
+    EGvec[ids[[1]]] <- 1
     EGvec <- factor(EGvec)
-    if(length(levels(EGvec)) == 1)
-        stop('the allEG gene universe cannot be the same as the provided ids ...')
-    if(species(orgdb) == 'Homo sapiens') dbname <- 'org.Hs.eg.db'
-    if(species(orgdb) == 'Mus musculus') dbname <- 'org.Mm.eg.db'
-    if(species(orgdb) == 'Drosophila melanogaster') dbname <- 'org.Dm.eg.db'
-    
-    GOdata <- new("topGOdata", ontology=ontology, allGenes=EGvec,
-                  annot=annFUN.org, mapping=dbname, nodeSize=minN)
-    res <- runTest(GOdata, algorithm="classic", statistic="fisher")
-    resdf <- GenTable(GOdata, classic=res, topNodes=50)
-    ann.genes <- genesInTerm(GOdata, whichGO=resdf$GO.ID)
-    Genes <- sapply(ann.genes,function (x) {intersect(x,ids)})  
-    Genes <- sapply(Genes,function(x) {c(paste(x,sep="",collapse = "/"))})
-    Genes <- as.data.frame(Genes)
-    resdf <- cbind(resdf,Genes)
-    resdf$Term <- Term(GOTERM[resdf$GO.ID])
-    pvals <- resdf$classic
-    pvals <- as.numeric(sub('<', '', pvals))
-    resdf$classic <- pvals
-    inds <- which(resdf$Annotated < maxN & pvals < Pthr)
-    if(length(inds) == 0) return(NULL)
-    resdf <- resdf[inds,]
-    if(is.numeric(showTerms))
-    {
-      if(showTerms > 50)
-        showTerms <- 50
-      if(showTerms > nrow(resdf))
-        showTerms <- nrow(resdf)
-      enrich_GO <- resdf[1:showTerms,]
-      enrich_GO$Term <- factor(enrich_GO$Term, levels=enrich_GO$Term)
-      enrich_GO$P_val <- -log10(as.numeric(enrich_GO$classic))
-      g.plot <- ggplot(enrich_GO, aes(Term, Significant, fill=P_val)) + geom_bar(stat='identity') + coord_flip()+ ylab("Number of Genes")
-      return(list(data=resdf,enrichplot=g.plot))
-    }
-    return(resdf)
-}
+    if (length(levels(EGvec)) == 1) 
+        stop("the allEG gene universe cannot be the same as the provided ids ...")
+    if (species(orgdb) == "Homo sapiens") 
+        dbname <- "org.Hs.eg.db"
+    if (species(orgdb) == "Mus musculus") 
+        dbname <- "org.Mm.eg.db"
+    if (species(orgdb) == "Drosophila melanogaster") 
+        dbname <- "org.Dm.eg.db"
+    GOdata <- new("topGOdata", ontology = ontology, allGenes = EGvec, 
+        annot = annFUN.org, mapping = dbname, nodeSize = minN)
+    topGOlist <- tryCatch(lapply(seq_along(ids), function(i) {
+        
+        EGvec <- rep(0, length(allEG))
+        names(EGvec) <- allEG
+        EGvec[ids[[i]]] <- 1
+        EGvec <- factor(EGvec)
+        GOdata <- updateGenes(GOdata, EGvec)
 
+        res <- runTest(GOdata, algorithm = "classic", statistic = "fisher")
+        resdf <- GenTable(GOdata, classic = res, topNodes = 50)
+        ann.genes <- genesInTerm(GOdata, whichGO = resdf$GO.ID)
+        Genes <- sapply(ann.genes, function(x) {
+            intersect(x, ids[[i]])
+        })
+        Genes <- sapply(Genes, function(x) {
+            c(paste(x, sep = "", collapse = "/"))
+        })
+        Genes <- as.data.frame(Genes)
+        resdf <- cbind(resdf, Genes)
+        resdf$Term <- Term(GOTERM[resdf$GO.ID])
+        pvals <- resdf$classic
+        pvals <- as.numeric(sub("<", "", pvals))
+        resdf$classic <- pvals
+        inds <- which(resdf$Annotated < maxN & pvals < Pthr)
+        if (length(inds) == 0) 
+            return(NULL)
+        resdf <- resdf[inds, ]
+        if (is.numeric(showTerms)) {
+            if (showTerms > 50) 
+                showTerms <- 50
+            if (showTerms > nrow(resdf)) 
+                showTerms <- nrow(resdf)
+            enrich_GO <- resdf[1:showTerms, ]
+            enrich_GO$Term <- factor(enrich_GO$Term, levels = enrich_GO$Term)
+            enrich_GO$P_val <- -log10(as.numeric(enrich_GO$classic))
+            g.plot <- ggplot(enrich_GO, aes(Term, Significant, fill = P_val)) + 
+                geom_bar(stat = "identity") + coord_flip() + ylab("Number of Genes")
+            return(list(data = resdf, enrichplot = g.plot))
+        }
+        return(resdf)
+    }), error=function(e) e)
+
+    if (length(topGOlist)==1) {
+        return(topGOlist[[1]]) 
+    } else {
+        return(topGOlist)
+    }
+}
